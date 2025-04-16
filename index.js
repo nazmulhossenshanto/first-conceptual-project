b74a6ab12901da2112792a4ff3a73901cff0f541
shanto <mdnazmulhossenshanto01@gmail.com>
2025-04-16 19:04:08 +0600

    added people list

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-console.log('Initial set up done.')
+console.log('Initial set up done.');
+const people = [
+    {
+        id: 1,
+        name: 'Shanto',
+        age: 30,
+        occupation: "Software Engineer",
+        contact: {
+            eamil: "shanto@gmail.com"
+        },
+        address: {
+            street: "123 Main st",
+            city: "San Francisco",
+            state: "CA",
+            zip: "20105"
+        },
+        hobbies: ["coding", "hiking", "Photography"],
+    },
+    {
+        id: 2,
+        name: 'Rakib',
+        age: 40,
+        occupation: "Software Engineer",
+        contact: {
+            eamil: "rakib@gmail.com"
+        },
+        address: {
+            street: "123 Main st",
+            city: "San Francisco",
+            state: "CA",
+            zip: "20105"
+        },
+        hobbies: ["coding", "hiking", "Photography"],
+    },
+    {
+        id: 3,
+        name: 'Rubel',
+        age: 50,
+        occupation: "Software Engineer",
+        contact: {
+            eamil: "rubel@gmail.com"
+        },
+        address: {
+            street: "123 Main st",
+            city: "San Francisco",
+            state: "CA",
+            zip: "20105"
+        },
+        hobbies: ["coding", "hiking", "Photography"],
+    },
+];
+
+const peopleList = document.getElementById('people-list');
+
+
+const renderPeopleList = (people) => {
+    people.map((person) => {
+        const li = document.createElement('li');
+        li.classList.add("text-red-400", "coursor-pointer");
+        li.textContent = person.name;
+        peopleList.appendChild(li)
+    })
+}
+renderPeopleList(people)
